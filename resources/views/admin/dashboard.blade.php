f064242be69a62e55d0a751eb1a804d49a62bfc6
Zainal Hasan <zhanang19@gmail.com>
2020-09-24 18:23:36 +0700

    pagination complete

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -44,7 +44,7 @@
                             <td>
                               Rp. {{ $item->general_price }}
                             </td>
-                            
+
                             <td class="text-right">
                               <div class="dropdown">
                                 <a class="btn btn-sm btn-icon-only text-light" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -59,35 +59,11 @@
                             </td>
                           </tr>
                       @endforeach
-                    
-                   
                   </tbody>
                 </table>
               </div>
               <div class="card-footer py-4">
-                <nav aria-label="...">
-                  <ul class="pagination justify-content-end mb-0">
-                    <li class="page-item disabled">
-                      <a class="page-link" href="#" tabindex="-1">
-                        <i class="fas fa-angle-left"></i>
-                        <span class="sr-only">Previous</span>
-                      </a>
-                    </li>
-                    <li class="page-item active">
-                      <a class="page-link" href="#">1</a>
-                    </li>
-                    <li class="page-item">
-                      <a class="page-link" href="#">2 <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="page-item"><a class="page-link" href="#">3</a></li>
-                    <li class="page-item">
-                      <a class="page-link" href="#">
-                        <i class="fas fa-angle-right"></i>
-                        <span class="sr-only">Next</span>
-                      </a>
-                    </li>
-                  </ul>
-                </nav>
+                {!! $products->links('partials.pagination') !!}
               </div>
             </div>
           </div>
--- resources/views/partials/pagination.blade.php
+++ resources/views/partials/pagination.blade.php
@@ -1,34 +1,58 @@
 @if ($paginator->hasPages())
 <nav aria-label="...">
     <ul class="pagination justify-content-end mb-0">
+        {{-- Previous Page Link --}}
         @if ($paginator->onFirstPage())
-        <li class="page-item disabled">
-            <a class="page-link" href="">
-                <i class="fas fa-angle-left"></i>
-                <span class="sr-only">@lang('pagination.previous')</span>
-            </a>
-        </li>
+            <li class="page-item disabled">
+                <a class="page-link" href="">
+                    <i class="fas fa-angle-left"></i>
+                    <span class="sr-only">@lang('pagination.previous')</span>
+                </a>
+            </li>
         @else
-        <li class="page-item disabled">
-            <a class="page-link" href="{{ $paginator->previousPageUrl() }}">
-                <i class="fas fa-angle-left"></i>
-                <span class="sr-only">@lang('pagination.previous')</span>
-            </a>
-        </li>
+            <li class="page-item">
+                <a class="page-link" href="{{ $paginator->previousPageUrl() }}">
+                    <i class="fas fa-angle-left"></i>
+                    <span class="sr-only">@lang('pagination.previous')</span>
+                </a>
+            </li>
         @endif
-        <li class="page-item active">
-            <a class="page-link" href="#">1</a>
-        </li>
-        <li class="page-item">
-            <a class="page-link" href="#">2 <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="page-item"><a class="page-link" href="#">3</a></li>
-        <li class="page-item">
-            <a class="page-link" href="#">
-                <i class="fas fa-angle-right"></i>
-                <span class="sr-only">Next</span>
-            </a>
-        </li>
+
+        {{-- Pagination Elements --}}
+        @foreach ($elements as $element)
+            {{-- "Three Dots" Separator --}}
+            @if (is_string($element))
+                <li class="page-item disabled" aria-disabled="true"><span class="page-link">{{ $element }}</span></li>
+            @endif
+
+            {{-- Array Of Links --}}
+            @if (is_array($element))
+                @foreach ($element as $page => $url)
+                    @if ($page == $paginator->currentPage())
+                        <li class="page-item active" aria-current="page"><span class="page-link">{{ $page }}</span></li>
+                    @else
+                        <li class="page-item"><a class="page-link" href="{{ $url }}">{{ $page }}</a></li>
+                    @endif
+                @endforeach
+            @endif
+        @endforeach
+
+        {{-- Next Page Link --}}
+        @if ($paginator->hasMorePages())
+            <li class="page-item">
+                <a class="page-link" href="{{ $paginator->nextPageUrl() }}">
+                    <i class="fas fa-angle-right"></i>
+                    <span class="sr-only">@lang('pagination.next')</span>
+                </a>
+            </li>
+        @else
+            <li class="page-item disabled">
+                <a class="page-link" href="">
+                    <i class="fas fa-angle-right"></i>
+                    <span class="sr-only">@lang('pagination.next')</span>
+                </a>
+            </li>
+        @endif
     </ul>
 </nav>
 @endif
