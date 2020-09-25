11afcad515d84dc5f50c8435f25436dc7fc88d2b
Zainal Hasan <zhanang19@gmail.com>
2020-09-25 19:54:39 +0700

    🎨 Fix Styling

--- app/Http/Controllers/User/ViewDashboardController.php
+++ app/Http/Controllers/User/ViewDashboardController.php
@@ -2,13 +2,13 @@
 
 namespace App\Http\Controllers\User;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class ViewDashboardController extends Controller
 {
     /**
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function __invoke(Request $request)
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,7 +11,6 @@ class UserController extends Controller
 
     public function index()
     {
-        
     }
 
     public function product()
